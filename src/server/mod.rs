162569acddbaa9772c5aac93e175f2e6a2d244c4
hopeyen <hopey1118@gmail.com>
2023-04-19 14:45:33 +0200

    feat: GraphQL for comparison results and query arg options

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,4 +1,5 @@
 use axum::{extract::Extension, middleware, routing::get, Router, Server};
+
 use std::future::ready;
 use std::net::SocketAddr;
 use std::str::FromStr;
@@ -11,19 +12,28 @@ use crate::attestation::LocalAttestationsMap;
 use crate::server::model::{build_schema, POIRadioContext};
 use crate::server::observability::{create_prometheus_recorder, track_metrics};
 use crate::server::routes::{graphql_handler, graphql_playground, health};
-use crate::shutdown_signal;
+use crate::{shutdown_signal, CONFIG};
 
 pub mod model;
 pub mod observability;
 pub mod routes;
 
+/// This function starts a API server at the configured server_host and server_port
 pub async fn run_server(
-    host: String,
-    port: u16,
     running_program: Arc<AtomicBool>,
     local_attestations: Arc<AsyncMutex<LocalAttestationsMap>>,
 ) {
     info!("Initializing HTTP server");
+    let host = CONFIG
+        .get()
+        .unwrap()
+        .lock()
+        .unwrap()
+        .server_host
+        .clone()
+        .unwrap_or(String::from("0.0.0.0"));
+    let port = CONFIG.get().unwrap().lock().unwrap().server_port.unwrap();
+
     let context = Arc::new(POIRadioContext::init(Arc::clone(&local_attestations)).await);
 
     let schema = build_schema(Arc::clone(&context)).await;
